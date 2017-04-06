89b3681fcb141b80256f7fe6201924dfe590aae4
hugo <hugorperrier@gmail.com>
2017-04-06 17:44:46 +0100

    Second Submission

--- src/UdacityCapstoneMain.py
+++ src/UdacityCapstoneMain.py
@@ -1,53 +1,107 @@
-import numpy as np
-import pandas as pd
-import quandl
-import time
-import matplotlib.pyplot as plt
 from UdacityCapstoneFunctions import *
+from UdacityCapstonePrintout import *
 
-# Get a list of companies and tickers
-df = pd.read_csv('Companies.csv', names=['Ticker', 'Name'], comment='#')
-dict=zip(list(df.Ticker), list(df.Name))
-ticker_list = df.Ticker.tolist()
-cmpny_list = df.Name.tolist()
 
-# Print the list of comapnies
-print ",".join(cmpny_list)
+########## Set parameters ##############
 
-# Define train and test sets
-train = dataset("Train", '2014-01-01', '2015-12-31')
-test = dataset("Test", '2016-01-01', '2016-12-31')
+# Most parameters are defined as lists so we can use loops to vary them
 
-# Set the cmpny used for feature data
-train.get_cmpny_list(df.Ticker.tolist())
-test.get_cmpny_list(df.Ticker.tolist())
+# Training period
+train_start_date = '2009-01-01'
+train_end_date = '2015-12-31'
 
-# Get the cmpny data from Quandl
-train.get_Quandl_data()
-test.get_Quandl_data()
+# Test period
+test_start_date = '2016-01-01'
+test_end_date = '2016-06-31'
 
-# Get the NASDAQ 100 data (NDX)
-train.get_NDX_data()
-test.get_NDX_data()
+# List of companies used as features (from input file)
+input_file = "Quandl_NASDAQ.csv"
+company_df = pd.read_csv(input_file, names=['Ticker', 'Name'], comment='#')
+t_list = company_df.Ticker.tolist()
+ticker_list = [t_list]
 
-# Choose the features to be used for preditions
-feature_choice = ["AdjOpen", "AdjClose"]
+# Choose the type of stock data to be used for preditions
+sd_list = ["Open", "Close", "Variation", "High", "Low", "Split", "Ex-Dividend", "AdjOpen", "AdjClose", "AdjHigh", "AdjLow", "AdjVariation"]
+stock_data_list = [sd_list]
 
-# Choose how many days are used for prediction
-nHist = 3
 
-# Divide train data into training set and cross validation set
-train.create_features(feature_choice , nHist)
-test.create_features(feature_choice , nHist)
+# Choose how many previous days are used to make predictions
+#nHist_list = [1,2,3,4,5,6,7,8,9]
+nHist_list = [1]
 
-# Choose a ML model
-model = SVR(kernel='linear', C=1.0, epsilon=1.0)
+# Choose a classification model
+# Create classifiers
+#model_list = [LogisticRegression(), GaussianNB(), LinearSVC(C=1.0),RandomForestClassifier(n_estimators=100)]
+model_list = [LogisticRegression()]
 
-# Get model from train data
-model = train.train_model(model)
+# Choose whether NDX of previous days should be used as a feature
+use_NDX = True
 
-# Use model to predict test data
-train.predict_NDX(model)
-test.predict_NDX(model)
-train.plot(train.trueNDX, train.predictNDX)
-test.plot(test.trueNDX, test.predictNDX)
+# Choose how the parameters are printed out in the output file
+#print_mode = 'nHist' # Print nHist, train_score, cv_score, test_score
+#print_mode = 'Model' # Print model, train_score, cv_score, test_score
+#print_mode = 'Company' # Print company, train_score, cv_score, test_score
+#print_mode = 'Stock' # Print stock_data, train_score, cv_score, test_score
+#print_mode = 'General' # Print nCompanies, stock_data, nHist, tr_s, cv_s, te_s
+#print_mode = 'ExtendedGeneral' # Print listOfComp, s_d, nHist, tr_s, cv_s, te_s
+print_mode = False
+
+########## Create and initialise the train and test datasets ##############
+
+# Create train and test sets
+train = dataset("Train", train_start_date, train_end_date)
+test = dataset("Test", test_start_date, test_end_date)
+
+# Load all companies data from a file
+train.getAllCompanyData()
+test.getAllCompanyData(train)
+
+# Check if the companies requested as features are available in datasets
+ticker_list = check_company_list(ticker_list, train, verbose=False)
+
+# Create vectors to store accuracy scores
+params = []
+train_scores = []
+cv_scores = []
+test_scores = []
+
+########## Preprocess data and create features ##########
+
+for tickers, stock_data, nHist, model in [(tickers, stock_data, nHist, model) for tickers in ticker_list for stock_data in stock_data_list for nHist in nHist_list for model in model_list]:
+
+	# Select which companies are used as features
+	train.select_companies(tickers)
+	test.select_companies(tickers)
+
+	# Split datasets and create features from raw data
+	train.create_features(stock_data, nHist, use_NDX=use_NDX)
+	test.create_features(stock_data, nHist, use_NDX=use_NDX)
+
+	# Make sure feature Matrix is not empty
+	if len(train.features.columns == 0):
+		print "Empty"
+
+	# Print out feature matrix info 
+        print_feature_matrix_info(train, print_features=False)
+        print_feature_matrix_info(test, print_features=False)
+	
+########## Train classification model ##########
+
+	# Train model
+	model = train.train_model(model)
+	
+########## Get score for different datasets ##########
+
+	# Use model to predict test data
+	params.append(print_param(tickers,stock_data,nHist,model,print_mode))
+	train_scores.append(train.train_score)
+	cv_scores.append(train.cv_score)
+	test_scores.append(test.predict_NDX(model))
+
+if print_mode != False:
+	out_file = open('Result_%s_Score_%s.csv' %(print_mode, train.start_year), 'w')
+	result = zip(params, train_scores, cv_scores, test_scores)
+	result.sort(key = lambda x: x[2], reverse=True)
+	for a,b,c,d in result:
+		out_file.write("%s, %.3f, %.3f, %.3f\n" %(a, b, c, d))
+	out_file.close()
--- src/UdacityCapstoneNasdaqScrapping.py
+++ src/UdacityCapstoneNasdaqScrapping.py
@@ -9,16 +9,21 @@ import numpy as np
 import pandas as pd
 
 
+# Get list of NASDAQ companies from the web
 def get_NASDAQ_fromWeb(save = None):
+
+	# Dict with company names and tickers
 	ticker_dict = {}
 
+	# Open url and find the company names in it
 	for line in re.findall('\[\"[A-Z]+\".*?\",', urllib.urlopen('http://www.nasdaq.com/quotes/nasdaq-100-stocks.aspx').read()):
 		if (line.find("PRESENT") < 0):		
 			ticker_dict[re.findall('\".*?\"', line)[0].replace('"','')] = re.findall('\".*?\"', line)[1].replace('"','')
 
+	# Number of companies found
 	print len(ticker_dict)
 
-	
+	# Create 2 lists for company symbols and company names
 	symbols, names = np.array(sorted(list(ticker_dict.items()), key = lambda x: x[0])).T
 
 	#Save to file	
@@ -28,11 +33,18 @@ def get_NASDAQ_fromWeb(save = None):
 	
 	return symbols, names
 
+# Get list from a file
 def get_NASDAQ_fromFile():
+
+	# Open and read file
 	colnames = ["symbols", "names"]
 	dataNasdaq = pd.read_csv("NASDAQ.csv", names=colnames)
+
+	# Cleanup company names
 	dataNasdaq["symbols"] = dataNasdaq["symbols"].apply(lambda d: d.replace('"', ""))
 	dataNasdaq["names"] = dataNasdaq["names"].apply(lambda d: d.replace('"', ""))
+
+	# Create 2 lists for company symbols and company names
 	symbols = dataNasdaq.symbols.tolist()
 	names = dataNasdaq.names.tolist()
 	
